20a3b15ce3f93b2b0b084bcf1b2fc6566dc95f60
LupusLudensEst <gurovvic@gmail.com>
2021-11-14 00:20:18 -0500

    dt 14 nov 2021

--- specs/autch.spec.js
+++ specs/autch.spec.js
@@ -11,6 +11,13 @@ describe('Auth', function () {
             .click();
         await expect($('img[alt="avatarIcon"]'))
             .toBeDisplayed();
+        const a = await $("//div[@class='col-md-8']").getText(); // "//div[@class='col-md-8']"  'h4' '.col-md-8 > h4' // "//*[normalize-space(.) = 'Reports']" //*[.="Don’t have an account? Create one."] //*[normalize-space(.)="Don’t have an account? Create one."]
+
+
+        console.log(`$Text is here: ${a}`);
+        await  expect($("//div[@class='col-md-8']"))
+            .toHaveText('Reports')
+
     });
 });
 
